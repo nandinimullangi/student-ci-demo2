70a1e24e35742cc4f71e77804d2a6a59740279a8
nandinimullangi <168262292+nandinimullangi@users.noreply.github.com>
2025-09-15 15:31:32 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,4 @@
-package hello;
-public class main Hello {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Pipeline CI/CD!");
         
